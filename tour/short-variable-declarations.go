6aa33848b1951de503c5e53ffe3b13f173db9015
jamesxu <smelikecat@163.com>
2022-07-13 20:55:25 +0800

    10: zero values

--- tour/short-variable-declarations.go
+++ tour/short-variable-declarations.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func main() {
+func j_main() {
 	var i, j int = 1, 2
 	k := 3
 	c, python, java := true, false, "oh, no!"
---
+++ tour/zero.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/* Zero values
+
+The zero value is:
+	0 for numeric types;
+	false for the boolean type, and
+	"" (the empty string) for strings
+*/
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
