1dd0805204ae8843b7ddc2608124e35c6bda5dd7
jamesxu <smelikecat@163.com>
2022-07-13 16:20:46 +0800

    6.2: named return values -add comments

--- tour/named-result.go
+++ tour/named-result.go
@@ -2,6 +2,26 @@ package main
 
 import "fmt"
 
+/* named return values
+
+func func_name(param_name type ...) int {
+	return integernumber
+}
+func func_name(param_name type ...) (string, string) {
+	return "string1", "string2"
+}
+func func_name(param_name type ...) (x int, y int) {
+	x = 100
+	y = 200
+	return
+}
+func func_name(param_name type ...) (x, y int) {
+	x = 100
+	y = 200
+	return
+}
+
+*/
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
