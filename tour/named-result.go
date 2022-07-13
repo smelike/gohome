bf0be940588afef3f94c848e9a04e818ec0f69fd
jamesxu <smelikecat@163.com>
2022-07-13 16:28:06 +0800

    6.3: A eturn statement without arguments returns the named return values.

--- tour/named-result.go
+++ tour/named-result.go
@@ -25,9 +25,15 @@ func func_name(param_name type ...) (x, y int) {
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
+
+	/*
+		A return statement without arguments returns the named return values. This is known as a "naked" return.
+
+		Naked return statements should be used only in short functions, as with the example shown here. They can harm readability in longer functions.
+	*/
 	return
 }
 
 func main() {
-	fmt.Println(split(17))
+	fmt.Println(split(27))
 }
