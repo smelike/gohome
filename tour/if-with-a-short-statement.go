1ade4c1e638d4d121d3c33f104902aa24a9115cc
jamesxu <smelikecat@163.com>
2022-07-14 17:04:52 +0800

    if statement start with a short statement; variable only in scope until the end of the if.

--- tour/if-with-a-short-statement.go
+++ tour/if-with-a-short-statement.go
@@ -5,14 +5,17 @@ import (
 	"math"
 )
 
+// the if statement can start with a short statement to execute before the condition.
+// variables declared by the statement are only in scope until the end of the if.
 func pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
 	}
 	return lim
+	// return v // undefined: v
 }
 
-func if_shortened_main() {
+func main() {
 	fmt.Println(
 		pow(3, 2, 10), // 3^2 < 10 return v
 		pow(3, 3, 20), // 3^3 > 20 return lim
