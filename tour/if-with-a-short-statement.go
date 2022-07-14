d25808cf84b9d7b2419733ea9fb000e9dfbf38d5
jamesxu <smelikecat@163.com>
2022-07-14 17:24:32 +0800

    if-else

--- tour/if-with-a-short-statement.go
+++ tour/if-with-a-short-statement.go
@@ -7,7 +7,7 @@ import (
 
 // the if statement can start with a short statement to execute before the condition.
 // variables declared by the statement are only in scope until the end of the if.
-func pow(x, n, lim float64) float64 {
+func if_with_pow(x, n, lim float64) float64 {
 	if v := math.Pow(x, n); v < lim {
 		return v
 	}
@@ -15,7 +15,7 @@ func pow(x, n, lim float64) float64 {
 	// return v // undefined: v
 }
 
-func main() {
+func if_with_a_short_statement_main() {
 	fmt.Println(
 		pow(3, 2, 10), // 3^2 < 10 return v
 		pow(3, 3, 20), // 3^3 > 20 return lim
