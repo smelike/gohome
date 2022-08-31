56dee462393dcb9368bd172780a4b836c44179c5
jamesxu <jamesxu@code>
2022-08-31 22:26:39 +0800

    gobase5.go

---
+++ houlin/ch4/gobase5.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	names := []string{"Eric", "Harry", "Robert", "Jim", "Mark"}
+
+	for _, name := range names {
+		go func(who string) {
+			fmt.Printf("Hello %s.\n", who)
+		}(name)
+	}
+	time.Sleep(time.Millisecond)
+}
