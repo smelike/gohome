e8152b5541181a2b25de42074cfde8401af79e54
jamesxu <jamesxu@code>
2022-09-03 22:45:01 +0800

    channel convert  by function definition

--- houlin/ch4/chanconv.go
+++ houlin/ch4/chanconv.go
@@ -16,19 +16,24 @@ import "fmt"
 func main() {
 	var ok bool
 
-	// 双向通道
+	// 双向通道 -》 转换为 -》 接收单向通道
 	ch := make(chan int, 1)
 
 	_, ok = interface{}(ch).(<-chan int)
 	fmt.Println("chan int => <-chan int:", ok)
 
-	//发送通道
+	//发送单向通道 - 转换为 -》双向通道
 	sch := make(chan<- int, 1)
 	_, ok = interface{}(sch).(chan int)
 	fmt.Println("chan<- int => chan int:", ok)
 
-	// 接收通道
+	// 接收通道 -》 转换为 -》 双向通道
 	rch := make(<-chan int, 1)
 	_, ok = interface{}(rch).(chan int)
 	fmt.Println("<-chan int => chan int:", ok)
+
+	/*
+		利用函数声明将【双向通道】转换为【单向通道】的做法，只算是 Go 语言的一个语法糖。
+		不能利用函数声明把单向通道转换为双向通道，这样会得到一个编译错误。
+	*/
 }
