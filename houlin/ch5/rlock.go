8a6e8bfc66a1647d6aade27f11a217b7fa624bea
jamesxu <jamesxu@code>
2022-10-09 13:04:42 +0800

    sync.RWMutex

--- houlin/ch5/rlock.go
+++ houlin/ch5/rlock.go
@@ -19,8 +19,50 @@ func main() {
 			fmt.Printf("Unlocked for readings. [%d]\n", i)
 		}(i)
 	}
-	time.Sleep(time.Millisecond * 100)
+	time.Sleep(time.Millisecond * 100) // 睡眠等待运行时系统运行 goroutine
 	fmt.Println("Try to lock for writing...")
+	fmt.Println(time.Now())
 	rwm.Lock()
+	fmt.Println(time.Now())
 	fmt.Println("Locked for writing.")
 }
+
+/*
+go run .\rlock.go
+Try to lock for reading... [0]
+Locked for reading. [0]
+Try to lock for reading... [1]
+Locked for reading. [1]
+Try to lock for reading... [2]
+Locked for reading. [2]
+Try to lock for writing...
+Try to unlock for reading... [2]
+Unlocked for readings. [2]
+Try to unlock for reading... [1]
+Unlocked for readings. [1]
+Try to unlock for reading... [0]
+Unlocked for readings. [0]
+Locked for writing.
+
+// 读操作的锁定操作，使得主 goroutine 阻塞，因为读锁定未解锁，经过 time.Sleep(2s)  2s 后
+
+*/
+
+/*
+Try to lock for reading... [0]
+Locked for reading. [0]
+Try to lock for reading... [2]
+Try to lock for reading... [1]
+Locked for reading. [1]
+Locked for reading. [2]
+Try to lock for writing...
+2022-10-09 12:59:57.6116138 +0800 CST m=+0.109863001 [写锁定被阻塞的时间]
+Try to unlock for reading... [2]
+Unlocked for readings. [2]
+Try to unlock for reading... [1]
+Unlocked for readings. [1]
+Try to unlock for reading... [0]
+Unlocked for readings. [0]
+2022-10-09 12:59:59.5167679 +0800 CST m=+2.015013101 [写锁定操作成功的时间]
+Locked for writing.
+*/
