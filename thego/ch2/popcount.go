4d2c37188c2afdbc7424af978a5ad5e187d1fb71
jamesxu <jamesxu@code>
2022-11-02 18:08:59 +0800

    exercise

--- thego/ch2/popcount.go
+++ thego/ch2/popcount.go
@@ -32,3 +32,15 @@ Note that the range loop in init uses only the index; the value is unnecessary a
 need not be included.
 The loop could also have been written as `for i, _ :=range pc{}`
 */
+
+/*
+Exercise 2.3 Rewrite PopCount to use a loop instead of a single expression. Compare
+the performance of the two version.
+
+Exercise 2.4 Write a version of PopCount that counts bits by shifting its argument through 64
+bit position, testing the rightmost bit each time. Compare its performance to the table-lookup version.
+
+Exercise 2.5 The expression x&(x-1) clears the rightmost non-zero bit of x. Write a version of PopCount
+that counts bits by using this fact, and assess its performance.
+
+*/
