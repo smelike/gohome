1f1196f277a244621a12e65d86ebf8a55391e572
jamesxu <jamesxu@code>
2022-11-03 15:52:24 +0800

    UTF8-rune-byte

--- thego/ch2/strings.go
+++ thego/ch2/strings.go
@@ -8,4 +8,12 @@ func main() {
 	fmt.Println(len(s))
 	// 32 代表空格？
 	fmt.Println(s[6])
+
+	const GoUsage = `Go is a tool for managing Go source code.
+		Usage:
+			go command [arguments] \v \\\
+		
+	....`
+
+	fmt.Println(GoUsage)
 }
---
+++ thego/ch2/strings2.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+
+	s := "Hello, 世界" // 9 runes
+
+	fmt.Println(len(s)) // 13 bytes
+	fmt.Println(utf8.RuneCountInString(s))
+
+	for i := 0; i < len(s); {
+		// size, the number of bytes occupied by the UTF-8 encoding of r
+		r, size := utf8.DecodeRuneInString(s[i:])
+		fmt.Printf("%d\t%c - %d\n", i, r, size)
+		i += size
+	}
+
+	var rl = utf8.RuneCountInString(s)
+	for i := 0; i < rl; i++ {
+		fmt.Println(s[i])
+	}
+
+	for i, r := range s {
+		fmt.Printf("%d\t%[2]q\t%[2]d\n", i, r)
+	}
+}
