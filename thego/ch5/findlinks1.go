3802468c8687d4a5e039478e6351eec8ce2b0364
jamesxu <smelikecat@163.com>
2022-11-19 21:17:47 +0800

    func outline, recursive, fixed-size function call stack, variable-size stacks

--- thego/ch5/findlinks1.go
+++ thego/ch5/findlinks1.go
@@ -14,9 +14,10 @@ func main() {
 		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
 		os.Exit(1)
 	}
-	for _, link := range visit(nil, doc) {
+	/* for _, link := range visit(nil, doc) {
 		fmt.Println(link)
-	}
+	} */
+	outline(nil, doc)
 }
 
 // visit appends to links each link found in n and returns the result.
@@ -32,4 +33,20 @@ func visit(links []string, n *html.Node) []string {
 		links = visit(links, c)
 	}
 	return links
+}
+
+/*
+	To descend the tree for a node n, visit recursively calls itself for each of
+	n's children, which are held in the FirstChild linked list.
+*/
+
+// in outline
+func outline(stack []string, n *html.Node) {
+	if n.Type == html.ElementNode {
+		stack = append(stack, n.Data) // push tag
+		fmt.Println(stack)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		outline(stack, c)
+	}
 }
