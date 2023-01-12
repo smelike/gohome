12786aab529cefa845e73d565be07aa4696e01e8
jamesxu <smelikecat@163.com>
2023-01-12 18:53:59 +0800

    anonymous filed and struct

--- thego/ch6/code/trick.go
+++ thego/ch6/code/trick.go
@@ -3,7 +3,7 @@ package main
 import "sync"
 
 var (
-	mu      sync.Mutex
+	mu      sync.Mutex // guards mapping
 	mapping = make(map[string]string)
 )
 
@@ -11,5 +11,23 @@ func Loopup(key string) string {
 	mu.Lock()
 	v := mapping[key]
 	mu.Unlock()
+	return v
+}
+
+/*
+version 2
+*/
+
+var cache = struct {
+	sync.Mutex
+	mapping map[string]string
+}{
+	mapping: make(map[string]string), // initialize the map
+}
+
+func Lookup(key string) string {
+	cache.Lock()
+	v := cache.mapping[key]
+	cache.Unlock()
 	return v
 }
