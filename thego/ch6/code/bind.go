186d92d8416281645516cbf079f0cb968072519b
jamesxu <smelikecat@163.com>
2023-01-13 15:06:37 +0800

    method: receiver

---
+++ thego/ch6/code/bind.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"time"
+)
+
+type Rocket struct {
+	/*  */
+}
+
+func (r *Rocket) Launch() {
+	fmt.Println("Launch:", time.Now())
+}
+
+type Point struct {
+	X, Y float64
+}
+
+func (p Point) Distance(q Point) float64 {
+	return math.Hypot(p.X-q.X, p.Y-q.Y)
+}
+
+func (p *Point) ScaleBy(factor float64) {
+	p.X *= factor
+	p.Y *= factor
+}
+
+func main() {
+	// r := new(Rocket)
+	// 传入匿名函数
+	/* time.AfterFunc(2*time.Second, func() {
+		r.Launch()
+	}) */
+
+	// 直接使用方法“值”传入 AfterFunc
+	// time.AfterFunc(2*time.Second, r.Launch)
+
+	// time.Sleep(10 * time.Second)
+
+	p := Point{1, 2}
+	q := Point{4, 6}
+	distance := Point.Distance
+	fmt.Println(distance(p, q))
+	fmt.Printf("%T\n", distance)
+
+	scale := (*Point).ScaleBy
+	scale(&p, 2)
+	fmt.Println(p)
+	fmt.Printf("%T\n", scale)
+
+	// the go programming language 中函数和方法的区别是指有没有接收器，
+	// 而不像其他语言那样是指有没有返回值。
+}
--- thego/ch6/code/coloredpoint2.go
+++ thego/ch6/code/coloredpoint2.go
@@ -22,4 +22,19 @@ func main() {
 	q.Point = p.Point
 	p.ScaleBy(2)
 	fmt.Println(*p.Point, *q.Point)
+
+	m := geometry.Point{1, 2}
+	n := geometry.Point{4, 6}
+	distanceFromP := m.Distance // {1, 2}
+	fmt.Println(distanceFromP(n))
+	var origin geometry.Point          // {0, 0}
+	fmt.Println(distanceFromP(origin)) // [1-0+2^2-0]
+
+	scaleP := m.ScaleBy
+	scaleP(2)
+	fmt.Println(m)
+	scaleP(3)
+	fmt.Println(m)
+	scaleP(18)
+	fmt.Println(m)
 }
