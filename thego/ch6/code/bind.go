e9317690ba822ba38d33aee445ae3bb5cd21eab4
jamesxu <smelikecat@163.com>
2023-01-13 15:23:52 +0800

    方法表达式

--- thego/ch6/code/bind.go
+++ thego/ch6/code/bind.go
@@ -27,6 +27,23 @@ func (p *Point) ScaleBy(factor float64) {
 	p.Y *= factor
 }
 
+func (p Point) Add(q Point) Point { return Point{p.X + q.X, p.Y + q.Y} }
+func (p Point) Sub(q Point) Point { return Point{p.X - q.X, p.Y - q.Y} }
+
+type Path []Point
+
+func (path Path) TranslateBy(offset Point, add bool) {
+	var op func(p, q Point) Point
+	if add {
+		op = Point.Add
+	} else {
+		op = Point.Sub
+	}
+	for i := range path {
+		path[i] = op(path[i], offset)
+	}
+}
+
 func main() {
 	// r := new(Rocket)
 	// 传入匿名函数
