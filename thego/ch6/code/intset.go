981084818d6569d78704fbe57852b1b44d20da1d
jamesxu <smelikecat@163.com>
2023-01-15 18:37:41 +0800

    bit array: integer set

--- thego/ch6/code/intset.go
+++ thego/ch6/code/intset.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+)
+
 // Bit 数组
 
 /*
@@ -15,6 +20,7 @@ Go 语言中的集合一般会用 map[T]bool 形式来表示，T 代表元素类
 一个 bit 数组通常会用一个无符号数或称之为“字”的 slice 表示，每个元素的每一位都
 表示集合里的一个值。当集合的第 i 位被设置时，我们才说这个集合包含元素 i。
 
+// 一个字有 64 位，即是一个字有 8 个字节？
 因为每一个字都有 64 个二进制位，所以为了定位 x 的 bit 位，我们用了 x/64 的商
 作为字的下标，并且用 x%64 得到的值作为这个字内的 bit 的所在位置。
 
@@ -57,4 +63,68 @@ func (s *IntSet) UnionWith(t *IntSet) {
 			s.words = append(s.words, tword)
 		}
 	}
+}
+
+// String 方法绑定的是 *InSet(指针)
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ') // 写入空格
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// return the number of elements
+func (s *IntSet) Len() int {
+	return len(s.words)
+}
+
+// remove x from the set
+func (s *IntSet) Remove(x int) {
+	/* for _, word := range s.words {
+		if word == x {
+			delete(s.words, i)
+		}
+	} */
+}
+func (s *IntSet) Clear() {
+
+}
+
+func (s *IntSet) Copy() *IntSet {
+
+	return s.Copy()
+}
+
+var x, y IntSet
+
+func main() {
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(9), x.Has(123))
+
+	fmt.Println(&x)
+	fmt.Println(x.String())
+	fmt.Println(x)
 }
